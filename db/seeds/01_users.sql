e0a5df65e1c8cf217f1beed0730eb8e3c301bea1
Andrew Ho <andrew.gjh@gmail.com>
2022-02-12 16:07:22 -0500

    add users to db

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
--- INSERT INTO users (name) VALUES ('Alice');
--- INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (email, first_name, last_name, password) VALUES ('alice@gmail.com','Alice', 'Lee', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, first_name, last_name, password) VALUES ('kira@gmail.com','Kira','Go', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, first_name, last_name, password) VALUES ('andrew@gmail.com','Andrew', 'Ho', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, first_name, last_name, password) VALUES ('matthew@gmail.com','Matthew', 'Bednarski', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (email, first_name, last_name, password) VALUES ('vivian@gmail.com','Vivian', 'Trinh', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
