0f98cdab51a67c22e0c4c3d17e1145149e7c2109
Andrew Ho <andrew.gjh@gmail.com>
2022-02-11 21:26:22 -0500

    Create schema of database

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,44 @@
--- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+DROP TABLE IF EXISTS to_do_items CASCADE;
+DROP TABLE IF EXISTS reminders CASCADE;
+DROP TABLE IF EXISTS locations CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  icon_url VARCHAR(255)
+);
+
+CREATE TABLE to_do_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  content VARCHAR (500),
+  category_id VARCHAR (255) REFERENCES categories.id ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users.id ON DELETE CASCADE,
+  priority SMALLINT,
+  deadline DATE,
+  is_completed BOOLEAN default FALSE,
+  created_at TIMESTAMP,
+);
+
+CREATE TABLE reminders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  to_do_item_id INTEGER NOT NULL REFERENCES to_do_items.id ON DELETE CASCADE,
+  is_completed BOOLEAN,
+  created_at TIMESTAMP
+);
+
+CREATE TABLE locations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  city VARCHAR(100),
+  country VARCHAR(100),
+  user_id INTEGER NOT NULL REFERENCES users.id ON DELETE CASCADE
 );
