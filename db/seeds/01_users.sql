7fb373f662f0a1cb249316f97879378d774982a9
Andrew Ho <andrew.gjh@gmail.com>
2022-02-13 13:00:03 -0500

    Fix db schema

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,14 @@
 -- Users table seeds here (Example)
-INSERT INTO users (email, first_name, last_name, password) VALUES ('alice@gmail.com','Alice', 'Lee', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (email, first_name, last_name, password) VALUES ('kira@gmail.com','Kira','Go', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (email, first_name, last_name, password) VALUES ('andrew@gmail.com','Andrew', 'Ho', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (email, first_name, last_name, password) VALUES ('matthew@gmail.com','Matthew', 'Bednarski', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
-INSERT INTO users (email, first_name, last_name, password) VALUES ('vivian@gmail.com','Vivian', 'Trinh', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO locations (city, country)
+VALUES ('Toronto', 'Canada');
+INSERT INTO locations (city, country)
+VALUES ('Montreal', 'Canada');
+INSERT INTO locations (city, country)
+VALUES ('Vancouver', 'Canada');
+
+INSERT INTO users (email, first_name, last_name, password,location_id) VALUES ('alice@gmail.com','Alice', 'Lee', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',2);
+INSERT INTO users (email, first_name, last_name, password,location_id) VALUES ('kira@gmail.com','Kira','Go', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',3);
+INSERT INTO users (email, first_name, last_name, password,location_id) VALUES ('andrew@gmail.com','Andrew', 'Ho', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',1);
+INSERT INTO users (email, first_name, last_name, password,location_id) VALUES ('matthew@gmail.com','Matthew', 'Bednarski', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',1);
+INSERT INTO users (email, first_name, last_name, password,location_id) VALUES ('vivian@gmail.com','Vivian', 'Trinh', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',1);
 
