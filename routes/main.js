6b4d486c898c3e2066bcf53d0486443cda1895ae
Andrew Ho <andrew.gjh@gmail.com>
2022-02-15 09:38:30 -0500

    Create database query functions

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,26 @@
-// MAIN PAGE ROUTES
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const database = require('../database-functions');
 
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    res.render("main");
+module.exports = () => {
+  router.post("/to-do-items", (req, res) => {
+    const toDoItem = req.body.toDo;
+    //simulating grabbing userId (ATTENTION FOR LATER)
+    const userID = 1;
+    //simulating getting category ID (ADD IN SMART SORT FUNCTION LATER)
+    const categoryId = 5;
+    // inserting item into database
+    database.addToDoItem(toDoItem, categoryId, userID);
   });
- return router;
+
+  router.get('/to-do-items/:categoryid', (req,res)=>{
+
+    const categoryId = req.params.categoryid;
+    //  simulating grabbing userId (ATTENTION FOR LATER)
+    const userID = 1;
+    database.findItemsByCategory(userID, categoryId)
+    .then((items)=>{res.json(items)});
+  });
+  return router;
 };
