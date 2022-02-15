61499cfe12c9daeccdba4f6b6dd0bc3df7ba2029
Matthew <m.bednarski@gmail.com>
2022-02-15 21:08:56 +0000

    Create basic user update/logout page with routes

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,28 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
+// USER ROUTES
 
 const express = require('express');
 const { DatabaseError } = require('pg');
 const router  = express.Router();
+const cookieSession = require('cookie-session');
+
+router.use(cookieSession({
+  name: 'session',
+  keys: [process.env.KEY1, process.env.KEY2],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    if (!req.session.user) {
+      return res.redirect("../login");
+    }
     return res.render("user");
   });
+  router.post("/", (req, res) => {
+    // For now this does nothing, stretch task would be to actually update user info
+  })
   return router;
 };
 
